920e3c041ae4152a48a76951bd0df7c197c9147b
Julien Dubois <julien.dubois@gmail.com>
2015-08-05 22:10:27 +0200

    initial commit

---
+++ src/test/javascript/spec/components/auth/auth.services.spec.js
@@ -0,0 +1,45 @@
+'use strict';
+
+describe('Services Tests ', function () {
+
+    beforeEach(module('jhipsterApp'));
+
+    describe('Auth', function () {
+        var $httpBackend, spiedLocalStorageService, authService, spiedAuthServerProvider;
+
+        beforeEach(inject(function($injector, localStorageService, Auth, AuthServerProvider) {
+            $httpBackend = $injector.get('$httpBackend');
+            spiedLocalStorageService = localStorageService;
+            authService = Auth;
+            spiedAuthServerProvider = AuthServerProvider;
+            //Request on app init
+            $httpBackend.whenGET('scripts/app/main/main.html').respond({});
+            $httpBackend.whenGET('scripts/components/navbar/navbar.html').respond({});
+            var globalJson = new RegExp('i18n\/.*\/global.json')
+            var mainJson = new RegExp('i18n\/.*\/main.json');
+            $httpBackend.whenGET(globalJson).respond({});
+            $httpBackend.whenGET(mainJson).respond({});
+          }));
+        //make sure no expectations were missed in your tests.
+        //(e.g. expectGET or expectPOST)
+        afterEach(function() {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+        
+          it('should call LocalStorageService.clearAll on logout', function(){
+            //GIVEN
+            //Set spy
+            spyOn(spiedLocalStorageService, "clearAll").and.callThrough();
+
+            //WHEN
+            authService.logout();
+            //flush the backend to "execute" the request to do the expectedGET assertion.
+            $httpBackend.flush();
+
+            //THEN
+            expect(spiedLocalStorageService.clearAll).toHaveBeenCalled();
+          });
+
+    });
+});
