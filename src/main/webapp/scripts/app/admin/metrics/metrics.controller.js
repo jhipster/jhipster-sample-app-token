28bfa49ae8104e366b4fea72d406a8a96ded7542
Julien Dubois <julien.dubois@gmail.com>
2015-11-20 16:16:14 +0100

    automatic project update

--- src/main/webapp/scripts/app/admin/metrics/metrics.controller.js
+++ src/main/webapp/scripts/app/admin/metrics/metrics.controller.js
@@ -49,7 +49,7 @@ angular.module('sampleTokenApp')
                     size: 'lg',
                     resolve: {
                         threadDump: function() {
-                            return data;
+                            return data.content;
                         }
 
                     }
