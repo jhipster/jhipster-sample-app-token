58cf47b144e5931b21b25080e24e0b1c7711b4bb
Julien Dubois <julien.dubois@gmail.com>
2016-10-06 22:51:13 +0200

    automatic project update

--- src/main/java/io/github/jhipster/sample/config/SecurityConfiguration.java
+++ src/main/java/io/github/jhipster/sample/config/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package io.github.jhipster.sample.config;
 
 import io.github.jhipster.sample.security.*;
 import io.github.jhipster.sample.security.jwt.*;
+
+import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -40,10 +42,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Inject
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        try {
+            auth
+                .userDetailsService(userDetailsService)
+                    .passwordEncoder(passwordEncoder());
+        } catch (Exception e) {
+            throw new BeanInitializationException("Security configuration failed", e);
+        }
     }
 
     @Override
